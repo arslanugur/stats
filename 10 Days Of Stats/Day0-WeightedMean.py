3573cc1a7b52a915ed25fc79037cbd64fae097cd
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-02 21:53:37 +0300

    Create Day0-WeightedMean.py

---
+++ 10 Days Of Stats/Day0-WeightedMean.py
@@ -0,0 +1,13 @@
+#https://www.hackerrank.com/challenges/s10-weighted-mean/problem
+#!/bin/python3
+
+def weighted_mean(x, w):
+    arr = zip(x, w)
+    return sum([e[0] * e[1] for e in arr]) / sum(w)
+
+
+int(input())
+x = [int(e) for e in input().split()]
+w = [int(e) for e in input().split()]
+print(round(weighted_mean(x, w), 1))
+
