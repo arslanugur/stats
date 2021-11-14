35cc9c0da9659462a1dd7e541702056994379445
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-14 21:32:17 +0300

    Create 03. Functions.R

---
+++ R Programming/Basic/03. Functions.R
@@ -0,0 +1,13 @@
+# general syntax is: function(parameter1, parameter2, parameter3){code}
+
+Square <- function(x) {
+  return(x^2)
+}
+
+print(Square(5))
+print(Square(x=5)) # it is the same thing
+
+# Output
+[1] 25
+[1] 25
+
