85fd582848b1fdd5e62dd95288914e3d122ae7a2
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-14 21:38:04 +0300

    Create 09. Predefined Lists.R

---
+++ R Programming/Basic/09. Predefined Lists.R
@@ -0,0 +1,18 @@
+letters:
+# Output
+[1] "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s"
+[20] "t" "u" "v" "w" "x" "y" "z"
+
+
+month.abb
+# Output:
+[1] "Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct"
+[11] "Nov" "Dec"
+
+
+month.name
+# Output:
+[1] "January"   "February"  "March"     "April"     "May"       "June"     
+[7] "July"      "August"    "September" "October"   "November"  "December"
+
+
