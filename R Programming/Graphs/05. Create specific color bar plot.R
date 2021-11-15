ccc70f89c75bc7967c5a4624dcdc319411f17df7
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-15 10:22:10 +0300

    Create 05. Create specific color bar plot.R

---
+++ R Programming/Graphs/05. Create specific color bar plot.R
@@ -0,0 +1,6 @@
+testscores <- c(96, 71, 85, 92, 82, 78, 72, 81, 68, 61, 78, 86, 90)
+barplot(testscores, col="blue")
+
+# Output: <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAoMAAAKCCAI...
+
+
