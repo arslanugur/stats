3d4db28b51f51c4a9ad87cb1cd61cb0281566ecc
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-15 10:16:03 +0300

    Create 02. Picking specific columns out of a data frame.R

---
+++ R Programming/Data Structures/02. Picking specific columns out of a data frame.R
@@ -0,0 +1,12 @@
+#This code uses a dataset file with population estimates by the US Census Bureau
+head(population)
+
+# Output:
+NAME POPESTIMATE2009 NPOPCHG_2009
+1 United States       307006550      2631704
+2     Northeast        55283679       223483
+3       Midwest        66836911       241314
+4         South       113317879      1296857
+5          West        71568081       870050
+6       Alabama         4708708        31244
+
