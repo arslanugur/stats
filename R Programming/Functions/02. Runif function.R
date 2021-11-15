1f03708ef00221ce10fd5a4b2cafd6540afbacfb
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-15 10:01:17 +0300

    Create 02. Runif function.R

---
+++ R Programming/Functions/02. Runif function.R
@@ -0,0 +1,5 @@
+# the runif() function generates random numbers between 0 and 1. The first argument specifies how many numbers you want.
+runif(3)
+
+# Output: [1] 0.09930646 0.30622340 0.99445562
+
