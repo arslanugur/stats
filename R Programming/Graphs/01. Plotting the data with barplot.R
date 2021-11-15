1f303eb42cc465902a7958ec46e26d57d7e8b79a
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-15 10:19:00 +0300

    Create 01. Plotting the data with barplot.R

---
+++ R Programming/Graphs/01. Plotting the data with barplot.R
@@ -0,0 +1,4 @@
+barplot(table(floor(runif(200)*4)), xlab="Generated Numbers", ylab="Occurrences", border="red")
+
+# Output: <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAoMAAAKCCAIAAAFSV3TqAAAAAXNS...
+
