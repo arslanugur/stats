28c68dc6acf5e6f28a52c363721d6666e8096651
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-15 10:07:14 +0300

    Create 09. Summary Function.R

---
+++ R Programming/Functions/09. Summary Function.R
@@ -0,0 +1,8 @@
+# calculates a few values based on the data passed as the first argument. The exact values calculated depend on the class of the data.
+summary(1:10)
+
+# Output:
+Min.  1st Qu.  Median    Mean  3rd Qu.  Max. 
+1.00    3.25    5.50     5.50    7.75   10.00
+
+
